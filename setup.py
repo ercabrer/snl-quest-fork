b829271fa81149eaf6158082449409d67999c6e9
Eriel Cabrera <eecabre@sandia.gov>
2026-05-18 13:34:27 -0600

    added PyPI CI/CD workflow

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         "openai==1.86.0",
         "psutil==5.9.0",
         "GitPython==3.1.43",
-        'NodeGraphQt @ git+https://github.com/cancom84/NodeGraphQt-PySide6.git',
+        # 'NodeGraphQt @ git+https://github.com/cancom84/NodeGraphQt-PySide6.git',
         "notebook==7.5.5",
         "nbformat==5.10.4"
     ],
